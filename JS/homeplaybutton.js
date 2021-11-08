4399c2a96935817f88337dfc3e8369188593099d
DevvCreates <87363866+DevvCreates@users.noreply.github.com>
2021-11-08 18:42:17 -0500

    Add Website Files

---
+++ JS/homeplaybutton.js
@@ -0,0 +1,14 @@
+/* "Flight" Preview Play Button on The Home Page */
+
+var mySongFlight = document.getElementById('mySongFlight');
+var icon = document.getElementById('icon');
+
+icon.onclick = function(){
+  if(mySongFlight.paused){
+    mySongFlight.play();
+    icon.src = "media/pause-icon.png";
+  }else{
+    mySongFlight.pause();
+    icon.src = "media/play-icon2.png";
+  }
+}
---
+++ JS/tracks.js
@@ -0,0 +1,19 @@
+/* Tracks Page JS Functions */
+
+/*
+
+var mySongFlight = document.getElementById('mySongFlight');
+var icon = document.getElementById('icon');
+
+icon.onclick = function(){
+  if(mySongFlight.paused){
+    mySongFlight.play();
+    icon.src = "media/pause-icon.png";
+  }else{
+    mySongFlight.pause();
+    icon.src = "media/play-icon2.png";
+  }
+}
+
+*/
+
